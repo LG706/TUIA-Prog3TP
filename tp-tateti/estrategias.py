c063eeb1072487671135f68e09489c2ecc8358a3
AiupAgustin <agustinaiup@hotmail.com>
2026-04-16 20:12:47 -0300

    cambios en tateti, no funciona todavia

--- tp-tateti/estrategias.py
+++ tp-tateti/estrategias.py
@@ -52,6 +52,41 @@ def estrategia_minimax(tateti: Tateti, estado: List[List[str]]) -> Tuple[int, in
     # 2. Implementar el algoritmo minimax aquí
     # 3. La función debe retornar una tupla (fila, columna) con la mejor jugada
 
+    #desde aca agregue cosas con las funciones max y min comentados funciona pero nunca responde la ia con un movimiento
+    def MAX(tateti , estado): return valor
+        if problema.TEST-TERMINAL(estado) then
+            return problema.UTILIDAD(estado,MAX)
+        valor = -1
+        forall acción in problema.ACCIONES(estado) do
+            sucesor = problema.RESULTADO(estado, acción)
+            valor = max(valor,
+                        MIN(problema, sucesor)
+        return valor
+
+    def MIN(tateti, estado) return valor
+        if problema.TEST-TERMINAL(estado) then
+            return problema.UTILIDAD(estado,MAX)
+        valor = +1
+        forall acción in problema.ACCIONES(estado) do
+            sucesor = problema.RESULTADO(estado, acción)
+            valor = min(valor,
+                        MAX(problema, sucesor)
+        return valor
+
+    
+    if tateti.jugador(estado) == MAX:
+        sucs = {acción: estrategia_minimax-MIN(tateti, tateti.resultado(estado, acción))
+            for acción in tateti.acciones(estado)}
+        return max(sucs, key=sucs.get) # obtiene la clave con el mayor valor
+    if tateti.jugador(estado) == MIN:
+        sucs = {acción: estrategia_minimax-MAX(tateti, tateti.resultado(estado, acción))
+            for acción in tateti.acciones(estado)}
+        return min(sucs, key=sucs.get) # obtiene la clave con el menor valor
+
+    #Hasta aca, lo de abajo estaba y lo comente para no borrarlo
+    """
+    la comento para no borrarla
+
     raise NotImplementedError(
         "\n" + "="*60 +
         "\n🚫 ALGORITMO MINIMAX NO IMPLEMENTADO" +
@@ -65,3 +100,4 @@ def estrategia_minimax(tateti: Tateti, estado: List[List[str]]) -> Tuple[int, in
         "\n\nMientras tanto, use la 'Estrategia Aleatoria'." +
         "\n" + "="*60
     )
+    """
